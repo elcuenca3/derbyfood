eb763cecb4eeeef7257dbe93d123e1dd17f1e2f0
RommelOjeda <56703120+RommelOjeda@users.noreply.github.com>
2023-07-22 23:16:36 -0500

    ayudamedios

--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class ObjectSpawner : MonoBehaviour
 {
     public GameObject[] objectsToSpawn;
-    public float spawnDelay = 10f;
-
+    public GameObject[] powerUpsToSpawn;
+    public float spawnDelay = 30f;
+    public float powerUpSpawnDelay = 60f;
     private bool playersHaveObject = false;
+
+    // Lista para almacenar las posiciones de los objetos instanciados
+    private System.Collections.Generic.List<Vector3> spawnPositions = new System.Collections.Generic.List<Vector3>();
 
     private void Start()
     {
         StartCoroutine(SpawnObjects());
+        StartCoroutine(SpawnPowerUps());
     }
 
     private IEnumerator SpawnObjects()
@@ -24,11 +29,20 @@ public class ObjectSpawner : MonoBehaviour
                 int randomIndex = Random.Range(0, objectsToSpawn.Length);
                 GameObject objectToSpawn = objectsToSpawn[randomIndex];
 
-                // Instanciar el objeto en una posición aleatoria
-                Vector3 spawnPosition = new Vector3(Random.Range(0f, 10f), 0f, Random.Range(0f, 10f));
+                // Generar una posición aleatoria dentro de un rango
+                Vector3 spawnPosition;
+                do
+                {
+                    spawnPosition = new Vector3(Random.Range(-5f, 90f), 10f, Random.Range(-5f, 90f));
+                } while (IsTooCloseToOtherSpawns(spawnPosition));
+
+                // Instanciar el objeto en la posición generada
                 Instantiate(objectToSpawn, spawnPosition, Quaternion.identity);
 
-                // Esperar 10 segundos
+                // Agregar la posición a la lista de posiciones de spawns
+                spawnPositions.Add(spawnPosition);
+
+                // Esperar el tiempo de spawnDelay
                 yield return new WaitForSeconds(spawnDelay);
             }
             else
@@ -39,8 +53,45 @@ public class ObjectSpawner : MonoBehaviour
         }
     }
 
+    private IEnumerator SpawnPowerUps()
+    {
+        while (true)
+        {
+            // Elegir un power-up aleatorio para spawnear
+            int randomIndex = Random.Range(0, powerUpsToSpawn.Length);
+            GameObject powerUpToSpawn = powerUpsToSpawn[randomIndex];
+
+            // Generar una posición aleatoria dentro de un rango
+            Vector3 spawnPosition;
+            do
+            {
+                spawnPosition = new Vector3(Random.Range(-5f, 90f), 10f, Random.Range(-5f, 90f));
+            } while (IsTooCloseToOtherSpawns(spawnPosition));
+
+            // Instanciar el power-up en la posición generada
+            Instantiate(powerUpToSpawn, spawnPosition, Quaternion.identity);
+
+            // Agregar la posición a la lista de posiciones de spawns
+            spawnPositions.Add(spawnPosition);
+
+            // Esperar el tiempo de powerUpSpawnDelay
+            yield return new WaitForSeconds(powerUpSpawnDelay);
+        }
+    }
+
     public void SetPlayersHaveObject(bool value)
     {
         playersHaveObject = value;
+    }
+
+    private bool IsTooCloseToOtherSpawns(Vector3 position)
+    {
+        // Verificar si la nueva posición está muy cerca de alguna posición de spawn anterior
+        foreach (Vector3 spawnPos in spawnPositions)
+        {
+            if (Vector3.Distance(spawnPos, position) < 30f) // Puedes ajustar el valor 5f según tu preferencia
+                return true;
+        }
+        return false;
     }
 }
--- Assets/Scripts/newnpc.cs
+++ Assets/Scripts/newnpc.cs
@@ -3,111 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class newnpc : MonoBehaviour
-/*
-{     public Transform target; // Referencia al objeto del carro controlado por el jugador
-    public float followDistance = 10f; // Distancia a la cual el NPC comenzará a seguir al jugador
-    public float moveSpeed = 5f; // Velocidad de movimiento del NPC
-    public float rotationSpeed = 2f; // Velocidad de rotación del NPC
-
-    private bool isFollowing = false;
-    private Vector3 randomTarget;
-
-    private void Start()
-    {
-        randomTarget = GetRandomTarget(); // Obtiene un objetivo aleatorio inicial
-    }
+{
+    public float speed = 2f; // Velocidad de movimiento del NPC
+    public Transform player; // Referencia al transform del jugador
 
     private void Update()
     {
-        if (target != null)
+        // Calculamos la dirección hacia el jugador
+        Vector3 directionToPlayer = (player.position - transform.position).normalized;
+
+        // Movemos al NPC hacia el jugador
+        transform.position += directionToPlayer * speed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Comprobamos si la colisión es con el jugador (u otro objeto específico, si lo deseas)
+        if (collision.gameObject.CompareTag("Player"))
         {
-            float distanceToTarget = Vector3.Distance(transform.position, target.position);
-
-            if (distanceToTarget <= followDistance)
-            {
-                isFollowing = true;
-            }
-            else
-            {
-                isFollowing = false;
-            }
-
-            if (isFollowing)
-            {
-                // Movimiento y rotación hacia el objetivo (carro del jugador)
-                Vector3 direction = target.position - transform.position;
-                Quaternion rotation = Quaternion.LookRotation(direction);
-                transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
-                transform.position += transform.forward * moveSpeed * Time.deltaTime;
-            }
-            else
-            {
-                // Movimiento aleatorio por la arena
-                float distanceToRandomTarget = Vector3.Distance(transform.position, randomTarget);
-
-                if (distanceToRandomTarget <= 1f)
-                {
-                    randomTarget = GetRandomTarget();
-                }
-
-                Vector3 direction = randomTarget - transform.position;
-                Quaternion rotation = Quaternion.LookRotation(direction);
-                transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
-                transform.position += transform.forward * moveSpeed * Time.deltaTime;
-            }
+            // Aquí puedes agregar el comportamiento que deseas que suceda cuando el NPC colisiona con el jugador.
+            // Por ejemplo, mostrar un mensaje, reproducir un sonido, quitar vida al jugador, etc.
+            Debug.Log("¡El NPC chocó con el jugador!");
         }
-    }
-
-    private Vector3 GetRandomTarget()
-    {
-        // Obtiene una posición aleatoria dentro de un área específica (arena)
-        float x = Random.Range(-10f, 10f);
-        float z = Random.Range(-10f, 10f);
-        return new Vector3(x, 0f, z);
-    }
-}
-*/
-
-{
-    public float speed = 5f; // Velocidad de movimiento de los NPC.
-    public float collisionDistance = 2f; // Distancia a la que el NPC detecta a otros autos y evita chocar.
-
-    private Transform target; // El objetivo del NPC (el punto central del área de movimiento).
-
-    void Start()
-    {
-        // Buscamos el objeto que representa el punto central del área de movimiento.
-        target = GameObject.FindGameObjectWithTag("NPCArea").transform;
-    }
-
-    void Update()
-    {
-        // Calculamos la distancia entre el NPC y el punto central del área de movimiento.
-        float distanceToTarget = Vector3.Distance(transform.position, target.position);
-
-        if (distanceToTarget <= collisionDistance)
-        {
-            // Si estamos cerca del punto central, activamos el comportamiento de choque.
-            MoveTowards(target.position);
-        }
-        else
-        {
-            // Si estamos lejos, seguimos corriendo por la arena o chocando con otros NPC.
-            // Aquí puedes implementar un movimiento aleatorio o algún comportamiento específico.
-            MoveRandomly();
-        }
-    }
-
-    void MoveTowards(Vector3 targetPosition)
-    {
-        // Movemos al NPC hacia el objetivo (el punto central del área de movimiento).
-        Vector3 moveDir = (targetPosition - transform.position).normalized;
-        transform.position += moveDir * speed * Time.deltaTime;
-    }
-
-    void MoveRandomly()
-    {
-        // Implementa aquí el comportamiento de movimiento aleatorio.
-        // Por ejemplo, puedes mover el NPC en una dirección aleatoria cada cierto tiempo.
     }
 }
